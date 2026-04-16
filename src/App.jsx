33ab3bf9e957e619817341009f47fac14bf2d6b2
Andrei <pantoja.andrei.d@gmail.com>
2026-04-16 13:45:30 +0800

    fix hashrouter basename bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <AuthProvider>
       <CartProvider> 
-        <HashRouter basename="/mina-oleshoppe">
+        <HashRouter>
           <Navbar />
           <Routes>
             <Route path="/" element={<StorePage />} />
