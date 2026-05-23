0d33f3bbab654c614c8adacbf49698fb412fe40c
Andrei <pantoja.andrei.d@gmail.com>
2026-05-23 18:05:21 +0800

    changes in database

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD079e1314eb66af1e167f7YOUR_ACTUAL_KEY_HERE", 
+  apiKey: "AIzaSyD079e1314eb66af1e167f7", 
   authDomain: "react-projects-46390.firebaseapp.com",
   projectId: "react-projects-46390",
   storageBucket: "react-projects-46390.firebasestorage.app",
